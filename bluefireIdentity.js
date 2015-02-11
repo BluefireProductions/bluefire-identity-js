eef57ba3774fa2ff8206246feab0fff7440aa054
Jonah Dempcy <jdempcy@gmail.com>
2015-02-11 15:01:42 -0800

    Bluefire Identity 1.0.4: Moved from using cookies to local storage for saving the JWT token; Also removes the JWT token from the URL

--- bluefireIdentity.js
+++ bluefireIdentity.js
@@ -6,8 +6,6 @@
  * @description
  * # BluefireIdentity
  *
- * Created by Timothy Jones on 2/20/14.
- *
  * Shared JS for Bluefire Identity service to auto-login an Angular app using Angular ui routing.
  * Include this module in your Angular app and it will automatically redirect you to the Bluefire SSO
  * server webpage which allows you to log in with your Google account. Once you have signed in, the SSO
@@ -22,24 +20,24 @@
  * client app 'token' on page reload at some point.
  */
 
-angular.module('BluefireIdentity', ['ngCookies'])
+angular.module('BluefireIdentity', ['LocalStorageModule'])
     .config(function ($httpProvider) {
       $httpProvider.interceptors.push('bluefireIdentity');
     })
     .run(function ($rootScope, $state, bluefireIdentity) {
       bluefireIdentity.setJwtFromUrl();
-      $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams){
-        if (toState.needAuth && !bluefireIdentity.isAuthorized()){
+      $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+        if (toState.needAuth && !bluefireIdentity.isAuthorized()) {
           // User isn’t authenticated
-          var toHref = $state.href(toState,toParams);
+          var toHref = $state.href(toState, toParams);
 
-          var full = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '') + location.pathname;
+          var full = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + location.pathname;
           var redirectString = 'https://bluefire-sso.appspot.com/?redirect_uri=' + encodeURIComponent(full) + encodeURIComponent(toHref);
           console.log('Redirecting to: ' + redirectString);
           window.location = redirectString;
           event.preventDefault();
         }
-      })
+      });
     });
 
 /**
@@ -52,35 +50,30 @@ angular.module('BluefireIdentity', ['ngCookies'])
  * Created by Timothy Jones on 2/20/14.
  */
 
-angular.module('BluefireIdentity').factory('bluefireIdentity', [ '$q', '$cookies', function ($q, $cookies) {
+angular.module('BluefireIdentity').factory('bluefireIdentity', [ '$q', 'localStorageService', function ($q, localStorageService) {
 
   // Helper function to know whether or not to add Authorization headers to an intercepted request.
   function isSecureUrl(url) {
-    if (url.startsWith('https://bluefire'))
-    {
+    if (url.startsWith('https://bluefire')) {
       return true;
     }
-
-    if (url.startsWith('http://localhost:'))
-    {
+    if (url.startsWith('http://localhost:')) {
       return true;
     }
-
     return false;
   }
 
   var factory = {};
 
-  factory.isAuthorized = function() {
-    if ($cookies.jwt === undefined || $cookies.jwt === null)
-    {
+  factory.isAuthorized = function () {
+    if (localStorageService.get('jwt') === undefined || localStorageService.get('jwt') === null) {
       return false;
     }
     return true;
-  }
+  };
 
   // $http Interceptor function
-  factory.responseError = function(response) {
+  factory.responseError = function (response) {
     var status = response.status;
     var url = response.config.url;
 
@@ -93,32 +86,30 @@ angular.module('BluefireIdentity').factory('bluefireIdentity', [ '$q', '$cookies
     } else {
       return $q.reject(response);
     }
-  }
+  };
 
   // $http Interceptor function
-  factory.request = function(config) {
-
-    if (typeof String.prototype.startsWith != 'function') {
-      String.prototype.startsWith = function (str){
+  factory.request = function (config) {
+    if (typeof String.prototype.startsWith !== 'function') {
+      String.prototype.startsWith = function (str) {
         return this.slice(0, str.length) === str;
       };
     }
 
     if (isSecureUrl(config.url) && factory.isAuthorized()) {
-      config.headers['Authorization'] = 'Bearer ' + $cookies.jwt;
+      config.headers['Authorization'] = 'Bearer ' + localStorageService.get('jwt');
     }
     return config;
+  };
 
-  }
-
-  factory.setJwtFromUrl = function(){
+  factory.setJwtFromUrl = function () {
     var QueryString = function () {
       // This function is anonymous, is executed immediately and
       // the return value is assigned to QueryString!
       var query_string = {};
       var query = window.location.search.substring(1);
       var vars = query.split('&');
-      for (var i=0;i<vars.length;i++) {
+      for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split('=');
         // If first entry with this name
         if (typeof query_string[pair[0]] === 'undefined') {
@@ -133,33 +124,32 @@ angular.module('BluefireIdentity').factory('bluefireIdentity', [ '$q', '$cookies
         }
       }
       return query_string;
-    } ();
+    }();
 
-    if (QueryString.token !== undefined)
-    {
-      $cookies.jwt = QueryString.token;
+    if (QueryString.token !== undefined) {
+      localStorageService.set('jwt', QueryString.token);
+      // Redirect user to the same page they are currently on, sans URL parameters
+      window.location = (window.location.origin + window.location.pathname + window.location.hash);
     }
   };
 
-  factory.getJwt = function() {
-    return $cookies.jwt;
+  factory.getJwt = function () {
+    return localStorageService.get('jwt');
   };
 
-  factory.setJwt = function(jwt) {
-    $cookies.jwt = jwt;
+  factory.setJwt = function (jwt) {
+    localStorageService.set('jwt', jwt);
   };
 
-  factory.clearJwt = function() {
-    delete $cookies['jwt'];
+  factory.clearJwt = function () {
+    localStorageService.set('jwt', undefined);
   };
 
-  factory.getAuthorizationHeader = function() {
-    if ($cookies.jwt === undefined || $cookies.jwt === null)
-    {
+  factory.getAuthorizationHeader = function () {
+    if (localStorageService.get('jwt') === undefined || localStorageService.get('jwt') === null) {
       return '';
     }
-
-    return 'Bearer ' + $cookies.jwt;
+    return 'Bearer ' + localStorageService.get('jwt');
   };
 
   return factory;
